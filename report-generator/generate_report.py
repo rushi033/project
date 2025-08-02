42e288d2d41dab3cfb1eb9031369309c43f66678
C1-89460-Rushikesh <rushiambalkar2@gmail.com>
2025-08-02 08:51:19 +0530

    first

--- report-generator/generate_report.py
+++ report-generator/generate_report.py
@@ -7,8 +7,7 @@ doc.add_heading('Semgrep Report (Static Scan)', level=1)
 with open('reports/semgrep_report.txt') as f:
     for line in f:
         doc.add_paragraph(line.strip())
-
-doc.add_heading('ZAP Report Summary (Dynamic Scan)', level=1)
+ #this is going to store the file        
 doc.add_paragraph('For detailed issues, please see the attached zap_report.html.')
 
 doc.save('reports/security_scan_report.docx')
